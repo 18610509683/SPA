3a03f7b7c6e649168e53031a5b91668e0082cdfe
18610509683 <2966568410@qq.com>
2021-02-03 11:15:07 +0800

    t

--- README-zh.md
+++ README-zh.md
@@ -1,1 +1,10 @@
-# 标题
+## git使用文档
+···
+│   list.txt
+│   README-zh.md
+│   README.md
+│   
+└───src
+    └───T0xCA
+            app.vue
+            
---
+++ src/T0xCA/app.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="apps">
+
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+	#apps {
+		box-sizing: border-box;
+		width: 100%;
+		height: 100%;
+		position: relative;
+		overflow: hidden;
+	}
+
+</style>
